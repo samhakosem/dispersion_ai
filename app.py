f430b83fdd4ecaa0e4d9ab0018121ea750e70ee6
muly.be@gmail.com <muly.be@gmail.com>
2024-04-16 12:23:07 +0300

    remove bg if needed

--- app.py
+++ app.py
@@ -2,24 +2,62 @@ import streamlit as st
 from PIL import Image, ImageEnhance
 import pandas as pd
 import numpy as np
+import requests
+import os
 
 st.title("Diamond Dispersion AI")
 
 class DiamondAreaCalculator:
     def __init__(self, image_path):
         self.image_path = image_path
+        self.no_bg_image_path = f'no-bg_{self.image_path}'
         self.img_array = None
         self.total_diamond_pixels = None
         self.load_image()
     
+    def remove_backgroup(self):
+        print("Removing Background...")
+        api_key = os.getenv('REMOVE_BG_API_KEY')
+        print("API Key:", api_key)
+
+        response = requests.post(
+            'https://api.remove.bg/v1.0/removebg',
+            files={'image_file': open(self.image_path, 'rb')},
+            data={'size': 'auto'},
+            headers={'X-Api-Key': api_key},
+        )
+        if response.status_code == requests.codes.ok:
+            with open(self.no_bg_image_path, 'wb') as out:
+                out.write(response.content)
+        else:
+            print("Error:", response.status_code, response.text)
+
+    
     def load_image(self):
         """Load the image from the file and convert it to a numpy array."""
         try:
-            img = Image.open(self.image_path)
+            # check if the image has an alpha channel
+            has_alpha_channel = None
+            tmp_img = Image.open(self.image_path)
+            tmp_image_array = np.array(tmp_img)
+            if tmp_image_array.shape[2] < 4:
+                has_alpha_channel = False
+            else:
+                has_alpha_channel = True
+            if not has_alpha_channel:
+                self.remove_backgroup()
+                img = Image.open(self.no_bg_image_path)
+            else:
+                img = tmp_img
             # Enhance the image by increasing saturation
-            enhancer = ImageEnhance.Color(img)
-            saturation_maximized = enhancer.enhance(5.0)  # Increasing saturation by a factor of 5            
-            image_array = np.array(saturation_maximized)
+            enhance_image = False  # Set this flag to True if you want to enhance the image, False otherwise
+            if enhance_image:
+                enhancer = ImageEnhance.Color(img)
+                saturation_maximized = enhancer.enhance(5.0)  # Increasing saturation by a factor of 5
+                image_array = np.array(saturation_maximized)
+            else:
+                image_array = np.array(img)
+            
             self.img_array = image_array
             adjusted_image = Image.fromarray(image_array)
             print("Image Loaded Successfully.")
